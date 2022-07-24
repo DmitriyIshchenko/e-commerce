47590bfb95b49b6dd76d0f59595cc153fd36318f
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2022-07-24 18:10:11 +0300

    Style header component

--- src/common/Header.js
+++ src/common/Header.js
@@ -4,14 +4,20 @@ import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as LeftSvg } from '../assets/system icon/24px/Left.svg';
 
+import styles from './Header.module.scss';
+
 export default function Header({ title }) {
   const navigate = useNavigate();
   return (
-    <header>
-      <button type='button' onClick={() => navigate(-1)}>
+    <header className={styles.header}>
+      <button
+        className={styles.btnBack}
+        type='button'
+        onClick={() => navigate(-1)}
+      >
         <LeftSvg />
       </button>
-      {title}
+      <p>{title}</p>
     </header>
   );
 }
