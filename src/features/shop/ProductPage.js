cf0f8befccd411b36d3a386e57c260a50a3297a8
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2022-07-26 17:14:01 +0300

    Style the product page

--- src/features/shop/ProductPage.js
+++ src/features/shop/ProductPage.js
@@ -3,12 +3,32 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { selectProductById } from './shopSlice';
 import StarRating from '../../common/StarRating';
-import { productAddedToCart } from '../cart/cartSlice';
+import { productAddedToCart, selectCartItemById } from '../cart/cartSlice';
 import DefaultPage from '../../common/DefaultPage';
 import Header from '../../common/Header';
+import PropTypes from 'prop-types';
 
-function ProductPage() {
+import styles from './ProductPage.module.scss';
+
+const AddToCartButton = ({ productId, price }) => {
   const dispatch = useDispatch();
+  const isInCart = !!useSelector((state) =>
+    selectCartItemById(state, productId)
+  );
+  const btnText = isInCart ? 'in cart' : 'add to cart';
+  return (
+    <button
+      type='button'
+      className={styles.btn}
+      onClick={() => dispatch(productAddedToCart(productId, price))}
+      disabled={isInCart}
+    >
+      {btnText}
+    </button>
+  );
+};
+
+export default function ProductPage() {
   const { productId } = useParams();
   const product = useSelector((state) => selectProductById(state, productId));
 
@@ -17,32 +37,39 @@ function ProductPage() {
   }
 
   const { name, description, price, rating, images } = product;
+  const formattedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(+price);
 
   return (
     <>
       <Header title={name} />
-      <main>
+      <main className={styles.page}>
         <section>
-          <img src={images[0]} alt='' />
-          <h2>{name}</h2>
-          <StarRating rating={rating} />
-          <span>{price}</span>
+          <div className={styles.imgBox}>
+            <img src={images[0]} alt='' />
+          </div>
+
+          <div className={styles.info}>
+            <p className={styles.name}>{name}</p>
+            <StarRating rating={rating} size={16} />
+            <p className={styles.price}>{formattedPrice}</p>
+          </div>
         </section>
 
-        <section>
-          <h3>specification</h3>
+        <section className={styles.meta}>
+          <h2>specification</h2>
           <p>{description}</p>
         </section>
 
-        <button
-          type='button'
-          onClick={() => dispatch(productAddedToCart(productId, price))}
-        >
-          add to cart
-        </button>
+        <AddToCartButton price={price} productId={productId} />
       </main>
     </>
   );
 }
 
-export default ProductPage;
+AddToCartButton.propTypes = {
+  productId: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+};
